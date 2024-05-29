7e8c38718f2a9c7de7cb04c1a435f7dbe4b7927d
elfaouziabdellatif <elfaouziabdellatif@gmail.com>
2024-05-29 10:45:07 +0100

    create navbar and animation of nav

--- myportfolio/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import MainRoutes from './routes/MainRoutes'
-export default function App() {
-  return (
-    <MainRoutes/>
-  )
-}
--- myportfolio/src/components/Spinner/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import './Spinner.css'
-const Spinner = () => (
-    <div className="text-center">
-   <span className="loader"></span>
-</div>
-  );
-  
-  export default Spinner;
-  
